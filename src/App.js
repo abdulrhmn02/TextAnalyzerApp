dd4716ce9b8f0f34db4353ea2705eba7b4f269c1
Abdul Rahman <abdulrahman@Abduls-MacBook-Air.local>
2024-06-07 11:03:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+import Alert from "./components/Alert";
+import { useState } from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+
+
+
+function App() {
+  const [mode , setMode]= useState('light');
+  const[alert, setAlert]=useState(null);
+const showAlert = (message , type)=>{
+setAlert({
+  msg: message,
+  type: type
+})
+setTimeout(()=>{
+setAlert(null);
+},3000);
+}
+  
+
+ 
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor='grey';
+      showAlert("dark mode enabled" , "success")
+    
+      
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white';
+    showAlert("light mode enabled" , "success");
+      
+    }
+  }
+  
+
+  return (
+    <>
+      <HashRouter>
+        <Navbar
+          title="TextUtils"
+          aboutText="AboutUs"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+
+        <Alert alert={alert} />
+        <div className="container my-3" mode={mode}>
+          <Routes>
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter the text to analyze below"
+                  mode={mode}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </HashRouter>
+    </>
+  );
+}
+
+export default App;
+  
---
+++ src/components/TextForm.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    console.log("uppercase was clicked");
+    let newText1 = text.toUpperCase();
+    setText(newText1);
+  };
+
+  const handleOnChange = (event) => {
+    console.log("on change");
+    setText(event.target.value);
+  };
+
+  const handleLowClick = () => {
+    console.log("lowercase was clicked");
+    let newText2 = text.toLowerCase();
+    setText(newText2);
+    props.showAlert("converted to lowercase", "success");
+  };
+
+  const handleCapitalizeClick = () => {
+    console.log("capitalize first letters was clicked");
+    let newText3 = capitalizeSentences(text);
+    setText(newText3);
+  };
+  const handleCLearCLick = () => {
+    console.log("clear text was clicked");
+    let newText4 = "";
+    setText(newText4);
+  };
+  const handleCopyClick = () => {
+    console.log("copy text was clicked");
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+  const handleSpacesClick = () => {
+    let newText5 = text.split(/[ ]+/);
+    setText(newText5.join(" "));
+  };
+
+  const capitalizeSentences = (text) => {
+    const sentences = text.split(/([.!?])/).filter(Boolean);
+    let result = "";
+    for (let i = 0; i < sentences.length; i += 2) {
+      const sentence = sentences[i].trim();
+      const punctuation = sentences[i + 1] || "";
+      result += capitalizeFirstLetter(sentence) + punctuation + " ";
+    }
+    return result.trim();
+  };
+
+  const capitalizeFirstLetter = (sentence) => {
+    return sentence.charAt(0).toUpperCase() + sentence.slice(1);
+  };
+
+  const [text, setText] = useState("");
+
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <div className="mb-3">
+          <h2>{props.heading}</h2>
+          <textarea
+            className="form-control"
+            id="myBox"
+            rows="15"
+            onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
+            value={text}
+          ></textarea>
+          <button className="btn btn-primary my-3 mx-4" onClick={handleUpClick}>
+            Convert To UPPERCASE
+          </button>
+          <button
+            className="btn btn-primary my-3 mx-4"
+            onClick={handleLowClick}
+          >
+            Convert To lowercase
+          </button>
+          <button
+            className="btn btn-primary my-3 mx-4"
+            onClick={handleCapitalizeClick}
+          >
+            Capitalize First Letters
+          </button>
+
+          <button
+            className="btn btn-primary my-3 mx-4"
+            onClick={handleCopyClick}
+          >
+            Copy Text
+          </button>
+
+          <button
+            className="btn btn-primary my-3 mx-4"
+            onClick={handleSpacesClick}
+          >
+            Remove Extra Spaces
+          </button>
+          <button
+            className="btn btn-primary my-3 mx-4"
+            onClick={handleCLearCLick}
+          >
+            Clear Text
+          </button>
+        </div>
+      </div>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h2>Your Text Summary</h2>
+        <p>
+          Number of words:{" "}
+          {text.split(/\s+/).filter((word) => word.length > 0).length}
+        </p>
+        <p>Number of characters: {text.length}</p>
+        <p>
+          Number of sentences:{" "}
+          {
+            text.split(/[.!?]/).filter((sentence) => sentence.trim().length > 0)
+              .length
+          }
+        </p>
+      </div>
+    </>
+  );
+}
